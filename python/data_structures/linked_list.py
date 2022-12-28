972bebb4f045fdb773e29af2c0338403455dd45f
Angelos <kounavisangelo@gmail.com>
2022-12-27 19:31:31 -0500

    challenge 5

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,15 +1,35 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
 class LinkedList:
-    """
-    Put docstring here
-    """
 
     def __init__(self):
         # initialization here
-        pass
+        self.head = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def insert(self, value):
+        new_node = Node(value, self.head)
+        self.head = new_node
+
+    def includes(self, value):
+        node = self.head
+        while node is not None:
+            if node.value == value:
+                return True
+            node = node.next
+        return False
+
+    def to_string(self):
+        values = []
+        node = self.head
+        while node is not None:
+            values.append(str(node.value))
+            node = node.next
+        if len(values) == 0:
+            return "NULL"
 
 
 class TargetError:
