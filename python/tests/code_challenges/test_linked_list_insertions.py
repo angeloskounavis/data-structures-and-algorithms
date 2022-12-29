33d3521c4d2c1cc99470f0f6b5ede879332a9c20
Angelos <kounavisangelo@gmail.com>
2022-12-29 17:26:02 -0500

    challenge 6, 7

--- python/tests/code_challenges/test_linked_list_insertions.py
+++ python/tests/code_challenges/test_linked_list_insertions.py
@@ -2,7 +2,7 @@ import pytest
 from data_structures.linked_list import LinkedList, TargetError
 
 
-@pytest.mark.skip("TODO")
+
 def test_append():
     linked_list = LinkedList()
 
@@ -15,7 +15,7 @@ def test_append():
     assert str(linked_list) == "{ banana } -> { apple } -> { cucumber } -> NULL"
 
 
-@pytest.mark.skip("TODO")
+
 def test_insert_before():
     linked_list = LinkedList()
 
@@ -28,7 +28,7 @@ def test_insert_before():
     assert str(linked_list) == "{ banana } -> { cucumber } -> { apple } -> NULL"
 
 
-@pytest.mark.skip("TODO")
+
 def test_insert_before_first():
     linked_list = LinkedList()
 
@@ -39,7 +39,7 @@ def test_insert_before_first():
     assert str(linked_list) == "{ cucumber } -> { apple } -> NULL"
 
 
-@pytest.mark.skip("TODO")
+
 def test_insert_after():
     linked_list = LinkedList()
 
@@ -52,7 +52,7 @@ def test_insert_after():
     assert str(linked_list) == "{ banana } -> { cucumber } -> { apple } -> NULL"
 
 
-@pytest.mark.skip("TODO")
+
 def test_insert_before_empty():
     linked_list = LinkedList()
 
@@ -60,7 +60,7 @@ def test_insert_before_empty():
         linked_list.insert_before("radish", "zucchinni")
 
 
-@pytest.mark.skip("TODO")
+
 def test_insert_before_missing():
     linked_list = LinkedList()
 
@@ -70,7 +70,7 @@ def test_insert_before_missing():
         linked_list.insert_before("radish", "zucchinni")
 
 
-@pytest.mark.skip("TODO")
+
 def test_insert_after_empty():
     linked_list = LinkedList()
 
@@ -78,7 +78,7 @@ def test_insert_after_empty():
         linked_list.insert_after("radish", "zucchinni")
 
 
-@pytest.mark.skip("TODO")
+
 def test_insert_after_missing():
     linked_list = LinkedList()
 
@@ -87,53 +87,4 @@ def test_insert_after_missing():
     with pytest.raises(TargetError):
         linked_list.insert_after("radish", "zucchinni")
 
-
-def test_exists():
-    assert LinkedList
-
-
-def test_instantiate():
-    assert LinkedList()
-
-
-def test_empty_head():
-    linked = LinkedList()
-    assert linked.head is None
-
-
-def test_populated_head():
-    linked = LinkedList()
-    linked.insert("apple")
-    assert linked.head.value == "apple"
-
-
-def test_to_string_empty():
-    linked_list = LinkedList()
-    assert str(linked_list) == "NULL"
-
-
-def test_to_string_single():
-    linked_list = LinkedList()
-    linked_list.insert("apple")
-    assert str(linked_list) == "{'apple'} -> NULL"
-
-
-def test_to_string_double():
-    linked_list = LinkedList()
-    linked_list.insert("apple")
-    assert str(linked_list) == "{'apple'} -> NULL"
-    linked_list.insert("banana")
-    assert str(linked_list) == "{'banana'} -> {'apple'} -> NULL"
-
-
-def test_includes_true():
-    linked_list = LinkedList()
-    linked_list.insert("apple")
-    linked_list.insert("banana")
-    assert linked_list.includes("apple")
-
-
-def test_includes_false():
-    linked_list = LinkedList()
-    linked_list.insert("apple")
 
