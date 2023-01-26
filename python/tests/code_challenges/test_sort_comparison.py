beed7059b30f72e03622c4b6f522680d0d44951e
Angelos <kounavisangelo@gmail.com>
2023-01-25 20:34:13 -0500

    challenge 28

---
+++ python/tests/code_challenges/test_sort_comparison.py
@@ -0,0 +1,101 @@
+import pytest
+from python.code_challenges.sort_comparison import sort_by_title, sort_by_year
+
+
+# @pytest.mark.skip("TODO")
+def test_sort_by_title_exists():
+    assert sort_by_title
+
+
+# @pytest.mark.skip("TODO")
+def test_sort_by_year_exists():
+    assert sort_by_year
+
+
+# @pytest.mark.skip("TODO")
+def test_sort_by_year(movies):
+    actual = sort_by_year(movies)
+    expected = ["The Intouchables", "Valkyrie", "Stardust", "Ratatouille", "City of God", "Memento", "The Shawshank Redemption", "Beetlejuice", "Crocodile Dundee", "The Cotton Club"]
+    assert actual == expected
+
+
+# @pytest.mark.skip("TODO")
+def test_sort_by_title(movies):
+    actual = sort_by_title(movies)
+    expected = ["Beetlejuice", "City of God", "The Cotton Club", "Crocodile Dundee", "The Intouchables", "Memento", "Ratatouille", "The Shawshank Redemption", "Stardust", "Valkyrie"]
+    assert actual == expected
+
+
+# @pytest.mark.skip("TODO")
+def test_sort_by_year_empty():
+    actual = sort_by_year([])
+    expected = []
+    assert actual == expected
+
+
+# @pytest.mark.skip("TODO")
+def test_sort_by_title_empty():
+    actual = sort_by_title([])
+    expected = []
+    assert actual == expected
+
+
+@pytest.fixture
+def movies():
+    movie_list = [
+        {
+            "title": "Beetlejuice",
+            "year": 1988,
+            "genres": ["Comedy", "Fantasy"],
+        },
+        {
+            "title": "The Cotton Club",
+            "year": 1984,
+            "genres": ["Crime", "Drama", "Music"],
+        },
+        {
+            "title": "The Shawshank Redemption",
+            "year": 1994,
+            "genres": ["Crime", "Drama"],
+        },
+        {
+            "title": "Crocodile Dundee",
+            "year": 1986,
+
+            "genres": ["Adventure", "Comedy"],
+        },
+        {
+            "title": "Valkyrie",
+            "year": 2008,
+            "genres": ["Drama", "History", "Thriller"],
+        },
+        {
+            "title": "Ratatouille",
+            "year": 2007,
+            "genres": ["Animation", "Comedy", "Family"],
+        },
+        {
+            "title": "City of God",
+            "year": 2002,
+
+            "genres": ["Crime", "Drama"],
+        },
+        {
+            "title": "Memento",
+            "year": 2000,
+
+            "genres": ["Mystery", "Thriller"],
+        },
+        {
+            "title": "The Intouchables",
+            "year": 2011,
+
+            "genres": ["Biography", "Comedy", "Drama"],
+        },
+        {
+            "title": "Stardust",
+            "year": 2007,
+            "genres": ["Adventure", "Family", "Fantasy"],
+        },
+    ]
+    return movie_list
---
+++ sorting/comparison/README.md
@@ -0,0 +1,21 @@
+# Blog Notes: Sort Comparison
+
+## Challenge
+
+- In the first half of this code challenge, you will write functions which sort domain objects.
+- Your functions will receive an array of Movie objects.
+- Each Movie object has a title (string), a year (number), and a list of genres (array of strings).
+- One function will sort the movies by most recent year first.
+- One function will sort the movies, alphabetical by title, but will ignore any leading “A”s, “An”s, or “The”s.
+- Test outputs for these functions, and an array of sample data, have been provided in test and movies.
+-
+## Approach & Efficiency
+
+ reading the instructions on canvas for this challenge.
+
+
+
+## Contributors
+- Jason
+- Ricardo
+- Aubrey
